94cf7d6a391a5073382d13319f88b0412752e1eb
talha7t <48598366+talha7t@users.noreply.github.com>
2020-07-23 16:55:20 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
-const totalBill = document.querySelector(".total-bill");
-const people = document.querySelector(".people");
-const billGennerator = document.getElementById("bill-generator");
+const billGenerator = document.getElementById("bill-generator");
 const billText = document.getElementById("bill");
 
-billGennerator.addEventListener("click", function (e) {
+/*--------Event Listeners----------------- */
+
+billGenerator.addEventListener("click", function (e) {
   e.preventDefault();
-  const bill = totalBill.value / people.value;
-  billText.innerText = `Payable bill = ${bill}`;
+
+  const totalBill = document.querySelector(".total-bill");
+  const people = document.querySelector(".people");
+
+  // checking that number of people is greater than one and is a whole number
+  if (people.value <= 0) {
+    billText.innerText = "Number of people can not be less than 1";
+  } else if (people.value % 1 !== 0) {
+    billText.innerText = "Number of people must be a whole number";
+  } else {
+    const bill = totalBill.value / people.value;
+    billText.innerText = `Payable bill = ${bill}`;
+  }
 });
